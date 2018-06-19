b70894ea5df9a94e88de03ad7027d4550ffa058e
Felix Morau <felixmorau@gmail.com>
2018-06-20 00:47:00 +0200

    added projects

--- felix-morau-website-2/app.js
+++ felix-morau-website-2/app.js
@@ -1,13 +1,73 @@
 new Vue({
     el: "#app",
     data: {
-        currentH1: "Felix Morau",
-        currentH3: "Education",
-        currentActive: "about",
+        currentH1: "My Projects",
+        currentH3: null,
+        currentActive: "myProjects",
         mainContent: "I went to school lol",
-        projects: [
-            {name: "Personal Website", github: "https://github.com/femosc2/felix-morau-website", description: "A personal website made with Vue", image: "images/background.jpg", language: "Vue.js"},
-            {name: "Vue Bloodborne", github: "https://github.com/femosc2/vue-bloodborne", description: "An inventory management game made with Vue", image: "images/bloodborne.jpg", language: "Vue.js"}
+        javaScriptProjects: [
+            {name: "Personal Website", 
+            github: "https://github.com/femosc2/felix-morau-website", 
+            description: "A personal website made with Vue", 
+            image: "images/background.jpg", 
+            language: "Vue.js"},
+
+            {name: "Vue Bloodborne", 
+            github: "https://github.com/femosc2/vue-bloodborne",
+            description: "An inventory management game made with Vue", 
+            image: "images/bloodborne.jpg", 
+            language: "Vue.js"},
+
+            {name: "Felix Inspo", 
+            github: "https://github.com/femosc2/felix-inspo", 
+            description: "An inspo site made with React", 
+            image: "images/florence.png", 
+            language: "React.js, CSS"},
+
+            {name: "Politweet", 
+            github: "https://github.com/femosc2/politweet", 
+            description: "Project for the course System Development and Project, created with React", 
+            image: "images/politweet.png", 
+            language: "React, CSS"}
+        ],
+        webProjects: [
+            {name: "Personal Website", 
+            github: "https://github.com/femosc2/felix-morau-website", 
+            description: "A personal website made with Vue", 
+            image: "images/background.jpg", 
+            language: "Vue.js"},
+
+            {name: "Felix Inspo", 
+            github: "https://github.com/femosc2/felix-inspo", 
+            description: "An inspo site made with React", 
+            image: "images/florence.png", 
+            language: "React.js, CSS"}
+        ],
+        pythonProjects: [
+            {name: "Bus Database",
+            github: "https://github.com/femosc2/db-project-mortfors", 
+            description: "Database assignment made with Python and PSQL", 
+            image: "images/background.jpg", 
+            language: "Python"},
+
+            {name: "Discord Bot", 
+            github: "https://github.com/femosc2/ia-discord-bot", 
+            description: "A discord bot for the IA discord channel made with Python.", 
+            image: "images/florence5.png", 
+            language: "Python"}
+        ],
+        universityProjects: [
+            {name: "Bus Database",
+            github: "https://github.com/femosc2/db-project-mortfors", 
+            description: "Database assignment made with Python and PSQL", 
+            image: "images/background.jpg", 
+            language: "Python"},
+
+            {name: "Politweet", 
+            github: "https://github.com/femosc2/politweet", 
+            description: "Project for the course System Development and Project, created with React", 
+            image: "images/politweet.png", 
+            language: "React, CSS"}
         ]
     },
 
@@ -18,6 +78,8 @@ new Vue({
             document.querySelector(".myProjects").classList.remove("active");
             document.querySelector(".contact").classList.remove("active");
             this.currentActive = "about";
+            this.scrollToTop();
+            
         },
         myProjects() {
             this.currentH1 = "My Projects";
@@ -25,6 +87,7 @@ new Vue({
             document.querySelector(".about").classList.remove("active");
             document.querySelector(".contact").classList.remove("active");
             this.currentActive = "myProjects";
+            this.scrollToTop();
         },
         contact() {
             this.currentH1 = "Contact";
@@ -32,7 +95,12 @@ new Vue({
             document.querySelector(".myProjects").classList.remove("active");
             document.querySelector(".about").classList.remove("active");
             this.currentActive = "contact";
+            this.scrollToTop();
         },
+        scrollToTop() {
+            document.body.scrollTop = 0;
+            document.documentElement.scrollTop = 0;
+        }
 }
 
 });
