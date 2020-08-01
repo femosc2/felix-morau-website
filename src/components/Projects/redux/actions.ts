16eba8fb21e1f455e60c949babf05135c1bd50c5
Felix Morau <felixmorau@gmail.com>
2020-08-01 15:57:46 +0200

    implement redux for modals

--- src/components/Projects/redux/actions.ts
+++ src/components/Projects/redux/actions.ts
@@ -1,4 +1,4 @@
-import { IProject } from './reducers';
+import { IProject, IProjectModal } from './reducers';
 
 export const SET_PROJECTS = 'SET_PROJECTS';
 export const SET_PROJECTS_FILTER = 'SET_PROJECTS_FILTER';
@@ -7,12 +7,13 @@ export const SET_PROJECTS_SKILLS = 'SET_PROJECTS_SKILLS';
 export const SET_FILTERED_PROJECTS_SKILLS = 'SET_FILTERED_PROJECTS_SKILLS';
 export const SET_PROJECTS_TYPES = 'SET_PROJECTS_TYPES';
 export const SET_FILTERED_PROJECTS_TYPES = 'SET_FILTERED_PROJECTS_TYPES';
-
+export const SET_PROJECT_MODAL = 'SET_PROJECT_MODAL';
 
 export type ProjectsAction =
 | { type: typeof SET_PROJECTS, projects: IProject[] }
 | { type: typeof SET_PROJECTS_FILTER, projectsFilter: IProject[] }
 | { type: typeof SET_PROJECT_FILTERS_VISIBILITY, visibility: boolean }
+| { type: typeof SET_PROJECT_MODAL, projectModal: IProjectModal }
 | { type: typeof SET_PROJECTS_SKILLS, projectsSkills: string[] }
 | { type: typeof SET_PROJECTS_TYPES, projectsTypes: string[] }
 | { type: typeof SET_FILTERED_PROJECTS_SKILLS, filteredProjectsSkills: string[] }
@@ -31,6 +32,11 @@ export const setProjectsFilter = (projectsFilter: IProject[]): ProjectsAction =>
 export const setFilterModalVisbility = (visibility: boolean): ProjectsAction => ({
   visibility,
   type: SET_PROJECT_FILTERS_VISIBILITY,
+});
+
+export const setProjectModal = (projectModal: IProjectModal): ProjectsAction => ({
+  projectModal,
+  type: SET_PROJECT_MODAL,
 });
 
 export const setProjectsSkills = (projectsSkills: string[]): ProjectsAction => ({
