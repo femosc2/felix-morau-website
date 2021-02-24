b4de3f7776bc3adc4c90f8fbfefa075ed6b5eacb
femosc2 <felixmorau@gmail.com>
2021-02-24 13:28:58 +0100

    under construction

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,19 +5,20 @@ import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import TopBarContainer from 'components/TopBar';
 import BottomBarContainer from 'components/BottomBar';
+import { ConstructionContainer } from '../Construction';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <div className="content" id="app-content">
-        <TopBarContainer />
+        {/* <TopBarContainer />
         <Switch>
           <Route exact path="/" component={ Structure } />
           <Route path={['/about', '/skills', '/contact', '/projects']}component={ Structure } />
-          {/* <Route path="/projects:id" component={ projectPage } /> */}
           <Route path="*" component={ ErrorPage } />
         </Switch>
-        <BottomBarContainer />
+        <BottomBarContainer /> */}
+        <ConstructionContainer />
       </div>
     </div>
   );
