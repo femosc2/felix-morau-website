d46c068ab7057db4e56b8816939322e81eab37d3
femosc2 <felixmorau@gmail.com>
2021-08-16 15:11:22 +0200

    add links to footer

--- client/src/components/SiteLayout/Footer/Footer.tsx
+++ client/src/components/SiteLayout/Footer/Footer.tsx
@@ -1,7 +1,9 @@
 import { useTranslation } from 'hooks/translation';
 import React from 'react';
+import { IoLogoInstagram, IoLogoLinkedin, IoLogoTwitter } from 'react-icons/io5';
 import styled from 'styled-components';
 import { COLORS } from 'variables/colors';
+import { MARGINS } from 'variables/margins';
 import { SHADOWS } from 'variables/shadows';
 
 interface IProps {
@@ -14,6 +16,17 @@ export const Footer: React.FC<IProps> = (props) => {
   return (
     <StyledFooter isTop={isAtTop} currentPage={currentPage}>
       <h3> {useTranslation('WebDeveloperDesigenr')} </h3>
+      <StyledSocialMediaLinks>
+        <a href={'target=\'_blank\' rel=\'noreferrer\' href={\'https://www.instagram.com/moraufelix/\'}'}>
+          <IoLogoInstagram size={25} />
+        </a>
+        <a href={'target=\'_blank\' rel=\'noreferrer\' href={\'https://twitter.com/moraufelix\'}'}>
+          <IoLogoTwitter size={25} />
+        </a>
+        <a href={'target=\'_blank\' rel=\'noreferrer\' href={\'https://www.linkedin.com/in/felixmorau/\'}'}>
+          <IoLogoLinkedin size={25} />
+        </a>
+      </StyledSocialMediaLinks>
     </StyledFooter>
   );
 };
@@ -33,4 +46,20 @@ color: ${COLORS.white};
 bottom: 0;
 z-index: 3;
 box-shadow: ${(props) => props.isTop && props.currentPage === 'about' ? '0 0 0 0' : SHADOWS.footerShadow};
+`;
+
+const StyledSocialMediaLinks = styled.section`
+margin: ${MARGINS.gutter};
+position: absolute;
+right: 0;
+> a {
+  transition: 0.2s;
+  color: ${COLORS.white};
+  padding: 0 10px 0 10px;
+  &:hover {
+    cursor: pointer;
+    color: ${COLORS.secondary};
+  }
+  }
+}
 `;
