34c5ffa856f9c107432d58a926371cf52a343635
femosc2 <felixmorau@gmail.com>
2021-08-17 00:08:29 +0200

    remove empty items from getProjects

--- server/src/api/Controllers/projectsController.ts
+++ server/src/api/Controllers/projectsController.ts
@@ -6,7 +6,7 @@ import { Project } from '../../Models/project';
 export const getProjects = ({ }, res: Response): Response<Project[]> | null => {
   try {
     db.ref('/projects').once('value').then((snapshot) => {
-      return res.status(200).send(snapshot.val());
+      return res.status(200).send(snapshot.val().filter((d: Project) => d !== null));
     });
   } catch(e) {
     return res.status(400).send(e);
