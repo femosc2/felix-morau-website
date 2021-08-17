2d06f9ab3dba2309d88d0c41ecef983ed9675f39
femosc2 <felixmorau@gmail.com>
2021-08-17 13:41:08 +0200

    skills implemented for mobile

--- client/src/components/Skills/components/Skill.tsx
+++ client/src/components/Skills/components/Skill.tsx
@@ -1,3 +1,4 @@
+import { useIsCompact } from 'hooks/isCompact';
 import { Skill } from 'models/skill';
 import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
@@ -14,6 +15,7 @@ export const SkillComponent: React.FC<IProps> = (props) => {
   const { skill } = props;
   const [isHovered, setIsHovered] = useState(false);
   const language = useSelector<IStore>(state => state.header.language);
+  const isCompact = useIsCompact();
   const descriptionLanguage = () => {
     switch(language) {
     case 'fr':
@@ -26,7 +28,11 @@ export const SkillComponent: React.FC<IProps> = (props) => {
   };
   return (
     <StyledSkillBar
-      comfortability={skill.comfortability} color={skill.color} onMouseEnter={() =>setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}
+      comfortability={skill.comfortability}
+      color={skill.color}
+      onMouseEnter={() =>setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      isCompact={isCompact}
     >
       <h4>{skill.name}</h4>
       {isHovered && <p>{descriptionLanguage()}</p>}
@@ -34,7 +40,7 @@ export const SkillComponent: React.FC<IProps> = (props) => {
   );
 };
 
-const StyledSkillBar = styled.figure<{comfortability: number, color: string}>`
+const StyledSkillBar = styled.figure<{comfortability: number, color: string, isCompact: boolean}>`
     background-color: ${COLORS.primary};
     width: ${(props) => props.comfortability}vw;
     color: ${COLORS.darkWhite};
@@ -43,14 +49,14 @@ const StyledSkillBar = styled.figure<{comfortability: number, color: string}>`
     margin-left: 0;
     &:hover {
         background-color: ${(props) => props.color};
-        height: 15vh;
+        height: ${(props) => props.isCompact ? '25vh' : '15vh'}
     }
     > h4 {
-        font-size: 30px;
+        font-size: ${(props) => props.isCompact ? '15px' : '30px'}
         margin: 0;
     }
     > p {
-        font-size: 25px;
+      font-size: ${(props) => props.isCompact ? '12px' : '25px'}
     }
 `;
 
